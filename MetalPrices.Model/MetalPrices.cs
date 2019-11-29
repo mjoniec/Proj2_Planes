648d7ad8543856c6513842d8aa16090d83fdd4f6
mjoniec <mart1n.joniec@gmail.com>
2019-11-29 01:02:44 +0100

    add metal prices model

---
+++ MetalPrices.Model/MetalPrices.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MetalPrices.Model
+{
+    public class MetalPrices
+    {
+        public List<MetalPriceDay> Prices { get; set; }
+    }
+
+    public class MetalPriceDay
+    {
+        public DateTime Date { get; set; }
+        public double Price { get; set; }
+    }
+}
