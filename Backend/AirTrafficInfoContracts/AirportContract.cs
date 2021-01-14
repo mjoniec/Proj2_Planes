569a2c01d4d0618878d3fd669dc76f510c100442
mjoniec <mart1n.joniec@gmail.com>
2021-01-14 23:19:17 +0100

    update contracts nuget

--- Backend/AirTrafficInfoContracts/AirportContract.cs
+++ Backend/AirTrafficInfoContracts/AirportContract.cs
@@ -5,6 +5,7 @@
         public string Name { get; set; }
         public double PositionX { get; set; }
         public double Positiony { get; set; }
+        public bool IsGoodWeather { get; set; }
         public Type Type => Type.Airport;
     }
 }
--- Backend/AirTrafficInfoContracts/PlaneContract.cs
+++ Backend/AirTrafficInfoContracts/PlaneContract.cs
@@ -1,10 +1,21 @@
-﻿namespace AirTrafficInfoContracts
+﻿using System;
+
+namespace AirTrafficInfoContracts
 {
     public class PlaneContract
     {
         public string Name { get; set; }
         public double PositionX { get; set; }
         public double Positiony { get; set; }
+        public DateTime PositionUpdateTime { get; set; }
+        public double SpeedInMetersPerSecond { get; set; }
+        public AirportContract DepartureAirport { get; set; }
+        public AirportContract DestinationAirport { get; set; }
         public Type Type => Type.Plane;
+
+        public void SetSpeedFromKilometersPerHour(int speedInKmH)
+        {
+            SpeedInMetersPerSecond = speedInKmH * 3600 * 1000;
+        }
     }
 }
