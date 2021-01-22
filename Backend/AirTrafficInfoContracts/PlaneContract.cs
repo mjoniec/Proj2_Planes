e271fd81220ddc0f011a7d6182f63454e1b6fa29
mjoniec <mart1n.joniec@gmail.com>
2021-01-22 23:32:19 +0100

    contract update

--- Backend/AirTrafficInfoContracts/PlaneContract.cs
+++ Backend/AirTrafficInfoContracts/PlaneContract.cs
@@ -7,14 +7,16 @@ namespace AirTrafficInfoContracts
         public string Name { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
-        public string Color { get; set; }
-        public double SymbolRotate { get; set; }
-        public string Symbol => "arrow";
         public DateTime PositionUpdateTime { get; set; }
         public double SpeedInMetersPerSecond { get; set; }
         public AirportContract DepartureAirport { get; set; }
         public AirportContract DestinationAirport { get; set; }
         public Type Type => Type.Plane;
+
+        //UI related
+        public string Color => DestinationAirport.Color;
+        public double SymbolRotate { get; set; }
+        public string Symbol => "arrow";
 
         //remove that method from here to some extension
         public void SetSpeedFromKilometersPerHour(int speedInKmH)
