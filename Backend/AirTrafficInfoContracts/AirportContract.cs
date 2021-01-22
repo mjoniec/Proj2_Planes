f99bb5c81570ad8a2efe97ae6378badf95fc9319
mjoniec <mart1n.joniec@gmail.com>
2021-01-22 23:14:17 +0100

    contracts update with UI related properties

--- Backend/AirTrafficInfoContracts/AirportContract.cs
+++ Backend/AirTrafficInfoContracts/AirportContract.cs
@@ -3,8 +3,11 @@
     public class AirportContract
     {
         public string Name { get; set; }
-        public double PositionX { get; set; }
-        public double Positiony { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Color { get; set; }
+        public double SymbolRotate { get; set; }
+        public string Symbol => "triangle";
         public bool IsGoodWeather { get; set; }
         public Type Type => Type.Airport;
     }
--- Backend/AirTrafficInfoContracts/PlaneContract.cs
+++ Backend/AirTrafficInfoContracts/PlaneContract.cs
@@ -5,14 +5,18 @@ namespace AirTrafficInfoContracts
     public class PlaneContract
     {
         public string Name { get; set; }
-        public double PositionX { get; set; }
-        public double Positiony { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Color { get; set; }
+        public double SymbolRotate { get; set; }
+        public string Symbol => "arrow";
         public DateTime PositionUpdateTime { get; set; }
         public double SpeedInMetersPerSecond { get; set; }
         public AirportContract DepartureAirport { get; set; }
         public AirportContract DestinationAirport { get; set; }
         public Type Type => Type.Plane;
 
+        //remove that method from here to some extension
         public void SetSpeedFromKilometersPerHour(int speedInKmH)
         {
             SpeedInMetersPerSecond = speedInKmH * 3600 * 1000;
