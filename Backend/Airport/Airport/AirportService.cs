06b894e1c498ada3b49bc67ceb9e9a5829ff5d6a
mjoniec <mart1n.joniec@gmail.com>
2021-02-06 14:28:56 +0100

    airport name setup per launch mathod, ps script

--- Backend/Airport/Airport/AirportService.cs
+++ Backend/Airport/Airport/AirportService.cs
@@ -19,7 +19,9 @@ namespace Airport
 
         public AirportService(IConfiguration configuration, IHostEnvironment hostEnvironment)
         {
-            var color = configuration.GetValue<string>("color");//required to install nuget: Microsoft.Extensions.Configuration.Binder
+            //required to install nuget: Microsoft.Extensions.Configuration.Binder
+            var name = configuration.GetValue<string>("name");
+            var color = configuration.GetValue<string>("color");
             var latitude = configuration.GetValue<string>("latitude");
             var longitude = configuration.GetValue<string>("longitude");
 
@@ -27,7 +29,7 @@ namespace Airport
             _httpClient = new HttpClient();
             _airportContract = new AirportContract
             {
-                Name = "Airport_" + _hostEnvironment.EnvironmentName + "_" + new Random().Next(1001, 9999).ToString(),
+                Name = AssignName(name),
                 Color = string.IsNullOrEmpty(color) ? "#" + new Random().Next(100000, 999999).ToString() : color,
                 Latitude = string.IsNullOrEmpty(latitude) ? new Random().Next(-150, 170) : Convert.ToDouble(latitude),
                 Longitude = string.IsNullOrEmpty(longitude) ? new Random().Next(-55, 70) : Convert.ToDouble(longitude)
@@ -54,6 +56,47 @@ namespace Airport
 
                 await Task.Delay(1100, stoppingToken);
             }
+        }
+
+        /// <summary>
+        /// we do not want the name on production to have anything other than city name or pilots name 
+        /// we also want to see more info easily on non production environments
+        /// </summary>
+        /// <param name="name"></param>
+        private string AssignName(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                if (_hostEnvironment.EnvironmentName == "Development")//manual on premises launch from visual studio
+                {
+                    name = "Airport_" + _hostEnvironment.EnvironmentName + "_" + new Random().Next(1001, 9999).ToString();
+                }
+                else if (_hostEnvironment.EnvironmentName == "Docker")
+                {
+                    name = "Error - AirportNameShouldHaveBeenGivenFor_" + _hostEnvironment.EnvironmentName + "_Environment_" + new Random().Next(1001, 9999).ToString();
+                }
+                else
+                {
+                    name = "Warning - Unpredicted Environment - Airport_" + _hostEnvironment.EnvironmentName + "_" + new Random().Next(1001, 9999).ToString();
+                }
+            }
+            else
+            {
+                if (_hostEnvironment.EnvironmentName == "Development")//on premises launch from ps script
+                {
+                    name += "_" + _hostEnvironment.EnvironmentName;
+                }
+                else if (_hostEnvironment.EnvironmentName == "Docker")
+                {
+                    //production name - expected to be displayed as given from docker compose
+                }
+                else
+                {
+                    name += "Warning - Unpredicted Environment - Airport_" + _hostEnvironment.EnvironmentName + "_" + new Random().Next(1001, 9999).ToString();
+                }
+            }
+
+            return name;
         }
     }
 }
