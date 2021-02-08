6ef10537c6e106c2e6c5dc060886a8eea062ce1b
mjoniec <mart1n.joniec@gmail.com>
2021-02-08 01:21:18 +0100

    ps1 cities

--- launchLocalhostOnPremises.ps1
+++ launchLocalhostOnPremises.ps1
@@ -10,18 +10,18 @@ $dirPlane = $currentDir + '\Backend\Plane\Plane';
 Start-Process -FilePath 'dotnet' -WorkingDirectory $dirAirTrafficInfoApi -ArgumentList 'run'
 Start-Sleep -s 4
 
-Start-Process -FilePath 'dotnet' -WorkingDirectory $dirAirport -ArgumentList 'run --name=PS_Airport1 --color=#FF0000 --latitude=50 --longitude=50'
+Start-Process -FilePath 'dotnet' -WorkingDirectory $dirAirport -ArgumentList 'run -name="New York" --color=#0000FF --latitude=40.68 --longitude=-74.17'
 Start-Sleep -s 2
-Start-Process -FilePath 'dotnet' -WorkingDirectory $dirAirport -ArgumentList 'run --name=PS_Airport2 --color=#00FF00 --latitude=30 --longitude=100'
+Start-Process -FilePath 'dotnet' -WorkingDirectory $dirAirport -ArgumentList 'run --name=London --color=#FF0000 --latitude=51.48 --longitude=-0.11'
 Start-Sleep -s 2
-Start-Process -FilePath 'dotnet' -WorkingDirectory $dirAirport -ArgumentList 'run --name=PS_Airport3 --color=#0000FF --latitude=10 --longitude=10'
+Start-Process -FilePath 'dotnet' -WorkingDirectory $dirAirport -ArgumentList 'run --name=Tokyo --color=#28FFB8 --latitude=35.67 --longitude=139.75'
 Start-Sleep -s 2
 
-Start-Process -FilePath 'dotnet' -WorkingDirectory $dirPlane -ArgumentList 'run --name=PS_Plane1'
+Start-Process -FilePath 'dotnet' -WorkingDirectory $dirPlane -ArgumentList 'run --name="Plane 1"'
 Start-Sleep -s 1
-Start-Process -FilePath 'dotnet' -WorkingDirectory $dirPlane -ArgumentList 'run --name=PS_Plane2'
+Start-Process -FilePath 'dotnet' -WorkingDirectory $dirPlane -ArgumentList 'run --name="Plane 2"'
 Start-Sleep -s 1
-Start-Process -FilePath 'dotnet' -WorkingDirectory $dirPlane -ArgumentList 'run --name=PS_Plane3'
+Start-Process -FilePath 'dotnet' -WorkingDirectory $dirPlane -ArgumentList 'run --name="Plane 3"'
 Start-Sleep -s 1
 
 #https://localhost:62462/api/AirTrafficInfo
