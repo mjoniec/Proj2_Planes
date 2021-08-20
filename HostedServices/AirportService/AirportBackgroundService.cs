e0307373de3acb11954749083a506f051350a32e
mjoniec <mart1n.joniec@gmail.com>
2021-08-21 01:21:27 +0200

    add airport project

---
+++ HostedServices/AirportService/AirportBackgroundService.cs
@@ -0,0 +1,91 @@
+﻿using Domain;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AirportService
+{
+    public class AirportBackgroundService : BackgroundService
+    {
+        private readonly Airport _airport;
+
+        private readonly HttpClient _httpClient;
+        private readonly IHostEnvironment _hostEnvironment;
+        private readonly string AirTrafficApiUpdateAirportInfoUrl;
+
+        public AirportBackgroundService(IConfiguration configuration, IHostEnvironment hostEnvironment)
+        {
+            //required to install nuget: Microsoft.Extensions.Configuration.Binder
+            var name = AssignName(configuration.GetValue<string>("name"));
+            var color = configuration.GetValue<string>("color");
+            var latitude = configuration.GetValue<string>("latitude");
+            var longitude = configuration.GetValue<string>("longitude");
+
+            _airport = new Airport(name, color, latitude, longitude);
+            _httpClient = new HttpClient();
+            _hostEnvironment = hostEnvironment;
+            AirTrafficApiUpdateAirportInfoUrl = configuration.GetValue<string>(nameof(AirTrafficApiUpdateAirportInfoUrl));
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await _airport.UpdateAirport();
+
+                await _httpClient.PostAsync(
+                    AirTrafficApiUpdateAirportInfoUrl, //TODO get rid of these http specific clients, export to shared kernel expose through interface
+                    new StringContent(JsonConvert.SerializeObject(_airportContract),
+                    Encoding.UTF8, "application/json"));
+
+                await Task.Delay(1100, stoppingToken);
+            }
+        }
+
+        /// <summary>
+        /// we do not want the name on production to have anything other than city name
+        /// we also want to see more info easily on non production environments
+        /// </summary>
+        /// <param name="name"></param>
+        private string AssignName(string name) //TODO this should go to hosted services 
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                if (_hostEnvironment.EnvironmentName == "Development")//manual on premises launch from visual studio
+                {
+                    name = "Airport_" + _hostEnvironment.EnvironmentName + "_" + new Random().Next(1001, 9999).ToString();
+                }
+                else if (_hostEnvironment.EnvironmentName == "Docker")
+                {
+                    name = "Error - AirportNameShouldHaveBeenGivenFor_" + _hostEnvironment.EnvironmentName + "_Environment_" + new Random().Next(1001, 9999).ToString();
+                }
+                else
+                {
+                    name = "Warning - Unpredicted Environment - Airport_" + _hostEnvironment.EnvironmentName + "_" + new Random().Next(1001, 9999).ToString();
+                }
+            }
+            else
+            {
+                if (_hostEnvironment.EnvironmentName == "Development")//on premises launch from ps script
+                {
+                    name += "_" + _hostEnvironment.EnvironmentName;
+                }
+                else if (_hostEnvironment.EnvironmentName == "Docker")
+                {
+                    //production name - expected to be displayed as given from docker compose
+                }
+                else
+                {
+                    name += "Warning - Unpredicted Environment - Airport_" + _hostEnvironment.EnvironmentName + "_" + new Random().Next(1001, 9999).ToString();
+                }
+            }
+
+            return name;
+        }
+    }
+}
