3bd2cf95e1a04829dfcd1add47542e98c5aca420
mjoniec <mart1n.joniec@gmail.com>
2021-08-21 01:16:16 +0200

    add plane service

--- HostedServices/PlaneService/Program.cs
+++ HostedServices/PlaneService/Program.cs
@@ -1,9 +1,6 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace PlaneService
 {
@@ -16,9 +13,13 @@ namespace PlaneService
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddCommandLine(args);
+                })
                 .ConfigureServices((hostContext, services) =>
                 {
-                    services.AddHostedService<Worker>();
+                    services.AddHostedService<PlaneBackgroundService>();
                 });
     }
 }
