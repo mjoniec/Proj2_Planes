47b587222b6a6fca740e4fb58426e6d1a2537d7c
mjoniec <mart1n.joniec@gmail.com>
2021-08-29 22:38:06 +0200

    refactor split airport loop host service #35

---
+++ HostedServices/PlaneService/Domain/PlaneLifetimeManager.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+using Utils;
+
+namespace PlaneService.Domain
+{
+    public class PlaneLifetimeManager
+    {
+        private readonly ILogger<PlaneLifetimeManager> _logger;
+        private readonly Plane _plane;
+        private readonly TrafficInfoHttpClient _trafficInfoHttpClient;
+        private readonly string TrafficInfoApiUpdatePlaneUrl;
+        private readonly string TrafficInfoApiGetAirportUrl;
+        private readonly string TrafficInfoApiGetAirportsUrl;
+
+        public PlaneLifetimeManager(string name, string trafficInfoApiUpdatePlaneUrl, 
+            string trafficInfoApiGetAirportUrl, string trafficInfoApiGetAirportsUrl)
+        {
+            var loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder.AddConsole();
+            });
+
+            _logger = loggerFactory.CreateLogger<PlaneLifetimeManager>();
+            _plane = new Plane(name);
+            _trafficInfoHttpClient = new TrafficInfoHttpClient();
+            TrafficInfoApiUpdatePlaneUrl = trafficInfoApiUpdatePlaneUrl;
+            TrafficInfoApiGetAirportUrl = trafficInfoApiGetAirportUrl;
+            TrafficInfoApiGetAirportsUrl = trafficInfoApiGetAirportsUrl;
+        }
+
+        public async Task Start()
+        {
+            _plane.StartPlane(await _trafficInfoHttpClient.GetCurrentlyAvailableAirports(TrafficInfoApiGetAirportsUrl));
+        }
+
+        public async Task Loop()
+        {
+            _logger.LogInformation(_plane.PlaneContract.Name + " update loop at " + DateTime.Now.ToString("G"));
+
+            _plane.UpdatePlane();
+
+            var airport = await _trafficInfoHttpClient.GetAirport(
+                TrafficInfoApiGetAirportUrl, _plane.PlaneContract.DestinationAirportName);
+
+            if (!airport.IsGoodWeather)
+            {
+                _logger.LogInformation("Plane's Destination Bad Weather");
+
+                await SelectNewDestinationAirport();
+            }
+
+            if (_plane.PlaneReachedItsDestination)
+            {
+                _logger.LogInformation("Plane Reached Its Destination");
+
+                await SelectNewDestinationAirport();
+            }
+
+            await _trafficInfoHttpClient.PostPlaneInfo(_plane.PlaneContract, TrafficInfoApiUpdatePlaneUrl);
+        }
+
+        private async Task SelectNewDestinationAirport()
+        {
+            _logger.LogInformation("SelectNewDestinationAirport");
+
+            var airports = await _trafficInfoHttpClient.GetCurrentlyAvailableAirports(TrafficInfoApiGetAirportsUrl);
+
+            _plane.SelectNewDestinationAirport(airports);
+        }
+    }
+}
--- HostedServices/PlaneService/PlaneBackgroundService.cs
+++ HostedServices/PlaneService/PlaneBackgroundService.cs
@@ -2,7 +2,6 @@
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using PlaneService.Domain;
-using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Utils;
@@ -12,69 +11,37 @@ namespace PlaneService
     public class PlaneBackgroundService : BackgroundService
     {
         private readonly ILogger<PlaneBackgroundService> _logger;
-        private readonly Plane _plane;
-        private readonly TrafficInfoHttpClient _trafficInfoHttpClient;
-        private readonly string TrafficInfoApiUpdatePlaneUrl;
-        private readonly string TrafficInfoApiGetAirportUrl;
-        private readonly string TrafficInfoApiGetAirportsUrl;
-
+        private readonly PlaneLifetimeManager _planeLifetimeManager;
+        
         public PlaneBackgroundService(
             ILogger<PlaneBackgroundService> logger,
             IConfiguration configuration, 
             IHostEnvironment hostEnvironment)
         {
-            //required to install nuget: Microsoft.Extensions.Configuration.Binder
-            var name = HostServiceNameSelector.AssignName("Plane", hostEnvironment.EnvironmentName, configuration.GetValue<string>("name"));
-
             _logger = logger;
-            _plane = new Plane(name);
-            _trafficInfoHttpClient = new TrafficInfoHttpClient();
-            
-            TrafficInfoApiUpdatePlaneUrl = configuration.GetValue<string>(nameof(TrafficInfoApiUpdatePlaneUrl));
-            TrafficInfoApiGetAirportUrl = configuration.GetValue<string>(nameof(TrafficInfoApiGetAirportUrl));
-            TrafficInfoApiGetAirportsUrl = configuration.GetValue<string>(nameof(TrafficInfoApiGetAirportsUrl));
+
+            var name = HostServiceNameSelector.AssignName("Plane", hostEnvironment.EnvironmentName, configuration.GetValue<string>("name"));
+            var trafficInfoApiUpdatePlaneUrl = configuration.GetValue<string>("TrafficInfoApiUpdatePlaneUrl");
+            var trafficInfoApiGetAirportUrl = configuration.GetValue<string>("TrafficInfoApiGetAirportUrl");
+            var trafficInfoApiGetAirportsUrl = configuration.GetValue<string>("TrafficInfoApiGetAirportsUrl");
+
+            _planeLifetimeManager = new PlaneLifetimeManager(name, trafficInfoApiUpdatePlaneUrl,
+                trafficInfoApiGetAirportUrl, trafficInfoApiGetAirportsUrl);
+
+            _logger.LogInformation("Created PlaneBackgroundService for: " + name);
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            _plane.StartPlane(await _trafficInfoHttpClient.GetCurrentlyAvailableAirports(TrafficInfoApiGetAirportsUrl));
+            await _planeLifetimeManager.Start();
 
-            //plane management logic should go to a separate domain lifecycle manager, exported as a tick for a loop holder object
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation(_plane.PlaneContract.Name + " update loop at " + DateTime.Now.ToString("G"));
-
-                _plane.UpdatePlane();
-
-                var airport = await _trafficInfoHttpClient.GetAirport(
-                    TrafficInfoApiGetAirportUrl, _plane.PlaneContract.DestinationAirportName);
-
-                if (!airport.IsGoodWeather)
-                {
-                    _logger.LogInformation("Plane's Destination Bad Weather");
-
-                    await SelectNewDestinationAirport();
-                }
-
-                if (_plane.PlaneReachedItsDestination)
-                {
-                    _logger.LogInformation("Plane Reached Its Destination");
-
-                    await SelectNewDestinationAirport();
-                }
-
-                await _trafficInfoHttpClient.PostPlaneInfo(_plane.PlaneContract, TrafficInfoApiUpdatePlaneUrl);
+                await _planeLifetimeManager.Loop();
                 await Task.Delay(1000, stoppingToken);
             }
-        }
 
-        private async Task SelectNewDestinationAirport()
-        {
-            _logger.LogInformation("SelectNewDestinationAirport");
-
-            var airports = await _trafficInfoHttpClient.GetCurrentlyAvailableAirports(TrafficInfoApiGetAirportsUrl);
-
-            _plane.SelectNewDestinationAirport(airports);
+            _logger.LogInformation("CancellationRequested");
         }
     }
 }
